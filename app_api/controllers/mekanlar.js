86652a1d0573e92afa32aaec087d8f77fd267f9c
Berk Şahutoğlu <115134027+berksahutoglu@users.noreply.github.com>
2022-11-21 19:25:44 +0300

    Add files via upload

--- app_api/controllers/mekanlar.js
+++ app_api/controllers/mekanlar.js
@@ -1,14 +1,14 @@
 var mongoose = require("mongoose");
 var Mekan = mongoose.model("mekan");
-const cevapOlustur = function (res,status,content){
+const cevapOlustur = function (res, status, content) {
     res.status(status).json(content);
 }
-var cevrimler = (function (){
-    var dunyaYariCap = 6371; // km
-    var radyan2Kilometre = function (radyan){
+var cevrimler = (function () {
+    var dunyaYariCap = 6371;
+    var radyan2Kilometre = function (radyan) {
         return parseFloat(radyan * dunyaYariCap);
     };
-    var kilometre2Radyan = function (mesafe){
+    var kilometre2Radyan = function (mesafe) {
         return parseFloat(mesafe / dunyaYariCap);
     };
     return {
@@ -16,24 +16,25 @@ var cevrimler = (function (){
         kilometre2Radyan: kilometre2Radyan,
     };
 })();
+
 const mekanlariListele = async (req, res) => {
     var boylam = parseFloat(req.query.boylam);
     var enlem = parseFloat(req.query.enlem);
     var koordinat = {
         type: "Point",
-        coordinates: [enlem,boylam],
+        coordinates: [enlem, boylam],
     };
     var geoOptions = {
-        distanceField:"mesafe",
+        distanceField: "mesafe",
         spherical: true,
     };
-    if ((!enlem && boylam !== 0) || (!enlem && boylam !== 0)){
-        cevapOlustur(res,404,{
-            "hata":"enlem ve boylsm zorunlu parametreler",
+    if ((!enlem && boylam !== 0) || (!enlem && boylam !== 0)) {
+        cevapOlustur(res, 404, {
+            "hata": "enlem ve boylam zorunlu parametreler",
         });
         return;
     }
-    try{
+    try {
         const sonuc = await Mekan.aggregate([
             {
                 $geoNear: {
@@ -41,9 +42,9 @@ const mekanlariListele = async (req, res) => {
                     ...geoOptions,
                 },
             },
-        ]);
-        const mekanlar = sonuc.map((mekan) =>{
-            return {
+        ])
+        const mekanlar = sonuc.map((mekan) => {
+            return{
                 mesafe: cevrimler.kilometre2Radyan(mekan.mesafe),
                 ad: mekan.ad,
                 adres: mekan.adres,
@@ -52,41 +53,43 @@ const mekanlariListele = async (req, res) => {
                 _id: mekan._id,
             };
         });
-        cevapOlustur(res,200,mekanlar);
-       }catch (e){
-        cevapOlustur(res,404,e);
+        cevapOlustur(res, 200, mekanlar);
+    } catch (e) {
+        cevapOlustur(res, 404, e);
     }
 };
-
-const mekanEkle=function(req,res){
-    cevapOlustur(res,200,{"durum":"başarılı"});
-    
-}
-const mekanGetir=function(req, res) { 
-    if(req.params && req.params.mekanid){
-        Mekan.findById(req.params.mekanid).exec(function (hata, mekan){
-            if (!mekan){
-                cevapOlustur(res,404, {"hata":"Böyle bir mekan yok!"});
-            }else if (hata){
-                cevapOlustur(res,404,{"hata": hata});
-            }else{
-                cevapOlustur(res,200,mekan);
+const mekanGetir = function (req, res) {
+    if (req.params && req.params.mekanid) {
+        Mekan.findById(req.params.mekanid).exec(function (hata, mekan) {
+            if (!mekan) {
+                cevapOlustur(res, 404, { "hata": "Böyle bir mekan yok!" });
+            }
+            else if (hata) {
+                cevapOlustur(res, 404, { "hata": hata });
+            }
+            else {
+                cevapOlustur(res, 200, mekan);
             }
         });
-    }else{
-        cevapOlustur(res,404,{"hata":"İstekte mekanid yok!"});
     }
-};
-const mekanGuncelle=function(req,res){
-    cevapOlustur(res,200,{"durum":"başarılı"});
+    else {
+        cevapOlustur(res, 404, { "hata": "İstekte mekan yok!" });
+    }
 }
-const mekanSil=function(req,res){
-    cevapOlustur(res,200,{"durum":"başarılı"});
+const mekanGuncelle = function (req, res) {
+    cevapOlustur(res, 200, { "durum": "başarılı" });
 }
-module.exports={
+const mekanEkle = function (req, res) {
+    cevapOlustur(res, 200, { "durum": "başarılı" });
+}
+const mekanSil = function (req, res) {
+    cevapOlustur(res, 200, { "durum": "başarılı" });
+}
+
+module.exports = {
     mekanlariListele,
     mekanEkle,
     mekanGetir,
+    mekanSil,
     mekanGuncelle,
-    mekanSil
 }
